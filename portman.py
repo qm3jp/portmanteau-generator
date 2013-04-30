91d578450949122bc4aab6ef7e5a01f20ad5ceed
Drew Benedetti <drewb@drewb.net>
2013-04-29 23:41:16 -0400

    rearrange

--- portman.py
+++ portman.py
@@ -6,21 +6,19 @@
 # TODO: use PyHyphen https://pypi.python.org/pypi/PyHyphen/0.9.3
 # TODO: other splitting algorithms? (nltk?)
 
-# XXX: apparently python is single pass, subs go at top or else there's an error
-def main ():
-    import sys
-    if len(sys.argv) != 3:
-        raise SystemExit("usage: %s <word> <word>" % (sys.argv[0]));
-
-    (pre, post) = sys.argv[1:]
-
-    words = smush(pre, post)
-    # TODO: nice formatting
-    print words
-
 def smush (a, b):
     return [ ''.join(a[:len(a)-i]) + ''.join(b[j:])
             for i in xrange(len(a))
             for j in xrange(len(b)) ]
 
-main()
+import sys
+
+# TODO: more arg validation
+if len(sys.argv) != 3:
+    raise SystemExit("usage: %s <word> <word>" % (sys.argv[0]));
+
+(pre, post) = sys.argv[1:]
+
+words = smush(pre, post)
+# TODO: nice formatting
+print words
